9aa78dd63d1632e1cbcf6ad2d84254a3a0ce35d6
Oliver Ni <oliver.ni@gmail.com>
2022-06-08 17:43:17 -0700

    Update pokedex command

--- module-pokedex/src/command/pokedex.rs
+++ module-pokedex/src/command/pokedex.rs
@@ -1,7 +1,8 @@
 use anyhow::{anyhow, Result};
+use inflector::Inflector;
 use poketwo_command_framework::{command, context::Context, group};
 use poketwo_protobuf::poketwo::database::v1::{
-    get_variant_request::Query, GetVariantRequest, Species, Variant,
+    get_variant_request::Query, GetVariantRequest, Species, SpeciesInfo, Variant,
 };
 use twilight_model::{
     channel::embed::Embed,
@@ -11,42 +12,72 @@ use twilight_util::builder::embed::{EmbedBuilder, EmbedFieldBuilder, ImageSource
 
 use crate::state::State;
 
+const FLAG_OFFSET: u32 = 0x1F1E6;
+const ASCII_OFFSET: u32 = 0x41;
+
+fn get_flag(country_code: &str) -> Result<String> {
+    let country_code = country_code.to_uppercase();
+    country_code
+        .to_ascii_uppercase()
+        .chars()
+        .map(|c| char::from_u32((c as u32) - ASCII_OFFSET + FLAG_OFFSET))
+        .collect::<Option<String>>()
+        .ok_or_else(|| anyhow!("Invalid country code"))
+}
+
+fn format_types(variant: &Variant) -> String {
+    let mut result: String = "".into();
+    for typ in &variant.types {
+        result.push_str(&typ.identifier.to_title_case());
+        result.push('\n');
+    }
+    result
+}
+
+fn format_region(species: &Species) -> Result<String> {
+    let generation = species.generation.as_ref().ok_or_else(|| anyhow!("Missing generation"))?;
+    let region = generation.main_region.as_ref().ok_or_else(|| anyhow!("Missing main region"))?;
+    Ok(region.identifier.to_title_case())
+}
+
+fn format_base_stats(variant: &Variant) -> String {
+    format!(
+        concat!(
+            "**HP:** {}\n",
+            "**Attack:** {}\n",
+            "**Defense:** {}\n",
+            "**Sp. Atk:** {}\n",
+            "**Sp. Def:** {}\n",
+            "**Speed:** {}"
+        ),
+        variant.base_hp,
+        variant.base_atk,
+        variant.base_def,
+        variant.base_satk,
+        variant.base_sdef,
+        variant.base_spd,
+    )
+}
+
+fn format_name(info: &SpeciesInfo) -> Result<String> {
+    let language = info.language.as_ref().ok_or_else(|| anyhow!("Missing language"))?;
+    Ok(format!("{} {}", get_flag(&language.iso3166)?, info.name))
+}
+
+fn format_names(species: &Species) -> Result<String> {
+    let mut result: String = "".into();
+    for info in &species.info {
+        result.push_str(&format_name(info)?);
+        result.push('\n');
+    }
+    Ok(result)
+}
+
+fn format_appearance(variant: &Variant) -> String {
+    format!("Height: {}\nWeight: {}", variant.height, variant.weight)
+}
+
 fn format_variant_embed(variant: &Variant) -> Result<Embed> {
-    fn format_base_stats(variant: &Variant) -> String {
-        format!(
-            concat!(
-                "**HP:** {}\n",
-                "**Attack:** {}\n",
-                "**Defense:** {}\n",
-                "**Sp. Atk:** {}\n",
-                "**Sp. Def:** {}\n",
-                "**Speed:** {}"
-            ),
-            variant.base_hp,
-            variant.base_atk,
-            variant.base_def,
-            variant.base_satk,
-            variant.base_sdef,
-            variant.base_spd,
-        )
-    }
-
-    fn format_names(species: &Species) -> Result<String> {
-        species
-            .info
-            .iter()
-            .map(|info| -> Result<String> {
-                let language = info.language.as_ref().ok_or_else(|| anyhow!("Missing language"))?;
-                Ok(format!("[{}] {}", language.identifier, info.name))
-            })
-            .reduce(|acc, item| Ok(format!("{}\n{}", acc?, item?)))
-            .ok_or_else(|| anyhow!("Missing info"))?
-    }
-
-    fn format_appearance(variant: &Variant) -> String {
-        format!("Height: {}\nWeight: {}", variant.height, variant.weight)
-    }
-
     let species = variant.species.as_ref().ok_or_else(|| anyhow!("Missing species"))?;
     let info = species.info.first().ok_or_else(|| anyhow!("Missing species info"))?;
 
@@ -58,8 +89,8 @@ fn format_variant_embed(variant: &Variant) -> Result<Embed> {
         embed = embed.description(flavor_text);
     }
 
-    embed = embed.field(EmbedFieldBuilder::new("Types", "Placeholder").inline());
-    embed = embed.field(EmbedFieldBuilder::new("Region", "Placeholder").inline());
+    embed = embed.field(EmbedFieldBuilder::new("Types", format_types(variant)).inline());
+    embed = embed.field(EmbedFieldBuilder::new("Region", format_region(species)?).inline());
     embed = embed.field(EmbedFieldBuilder::new("Catchable", "Placeholder").inline());
     embed = embed.field(EmbedFieldBuilder::new("Base Stats", format_base_stats(variant)).inline());
     embed = embed.field(EmbedFieldBuilder::new("Names", format_names(species)?).inline());
