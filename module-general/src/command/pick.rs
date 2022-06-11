29365cb22568862ac1a401fe6dab641c3c9a8848
Oliver Ni <oliver.ni@gmail.com>
2022-06-11 03:57:32 -0700

    Implement pick command

--- module-general/src/command/pick.rs
+++ module-general/src/command/pick.rs
@@ -1,9 +1,8 @@
-use std::collections::HashMap;
-
 use anyhow::{anyhow, Result};
+use maplit::hashmap;
 use poketwo_command_framework::command;
 use poketwo_protobuf::poketwo::database::v1::get_variant_request::Query;
-use poketwo_protobuf::poketwo::database::v1::GetVariantRequest;
+use poketwo_protobuf::poketwo::database::v1::{CreatePokemonRequest, CreateUserRequest, GetVariantRequest};
 use twilight_model::http::interaction::{InteractionResponse, InteractionResponseData, InteractionResponseType};
 
 use crate::Context;
@@ -22,13 +21,28 @@ pub async fn pick(
         .into_inner()
         .variant
         .ok_or_else(|| {
-            anyhow!(ctx.locale_lookup_with_args("pokemon-not-found", &HashMap::from([("query", starter.into())])))
+            anyhow!(ctx.locale_lookup_with_args("pokemon-not-found", &hashmap!("query" => starter.into())))
         })?;
+
+    let user_id = ctx.interaction.author_id().ok_or_else(|| anyhow!("Missing author"))?;
+    state.database.create_user(CreateUserRequest { id: user_id.into() }).await?;
+    state
+        .database
+        .create_pokemon(CreatePokemonRequest { user_id: user_id.into(), variant_id: variant.id, ..Default::default() })
+        .await?;
+
+    let name = variant
+        .species
+        .ok_or_else(|| anyhow!("Missing species"))?
+        .get_locale_info(&ctx.interaction.locale)
+        .ok_or_else(|| anyhow!("Missing info"))?
+        .name
+        .clone();
 
     Ok(InteractionResponse {
         kind: InteractionResponseType::ChannelMessageWithSource,
         data: Some(InteractionResponseData {
-            content: Some(format!("Picking {} {}", variant.id, variant.identifier)),
+            content: Some(ctx.locale_lookup_with_args("pick-response", &hashmap!("starter" => name.into()))),
             ..Default::default()
         }),
     })
