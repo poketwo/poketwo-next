70c82f61f8d15c0d5532d879ec2b69a4de93eea6
Oliver Ni <oliver.ni@gmail.com>
2022-06-17 19:58:27 -0700

    Add spawn embed

--- module-catching/src/spawning.rs
+++ module-catching/src/spawning.rs
@@ -2,11 +2,14 @@ use anyhow::Result;
 use bb8_redis::redis::AsyncCommands;
 use lapin::message::Delivery;
 use poketwo_command_framework::client::CommandClient;
+use poketwo_i18n::{Loader, LOCALES, US_ENGLISH};
 use poketwo_protobuf::poketwo::imgen::v1::GetSpawnImageRequest;
+use twilight_model::channel::embed::Embed;
 use twilight_model::gateway::payload::incoming::MessageCreate;
 use twilight_model::http::attachment::Attachment;
 use twilight_model::id::marker::{ChannelMarker, GuildMarker};
 use twilight_model::id::Id;
+use twilight_util::builder::embed::{EmbedBuilder, ImageSource};
 
 use crate::config::CONFIG;
 use crate::state::State;
@@ -57,8 +60,6 @@ async fn update_counter(
     let counter_key = format!("guild_counter:{}", guild_id);
     let val: u32 = conn.incr(&counter_key, 1).await?;
 
-    dbg!(&counter_key, val);
-
     if val >= CONFIG.activity_threshold {
         Ok(conn.del(&counter_key).await?)
     } else {
@@ -72,19 +73,35 @@ async fn get_spawn_image(client: &CommandClient<'_, State>, variant_id: i32) -> 
     Ok(response.into_inner().content)
 }
 
+fn make_spawn_embed() -> Result<Embed> {
+    // TODO: Localize message
+
+    let embed = EmbedBuilder::new()
+        .title(LOCALES.lookup(&US_ENGLISH, "pokemon-spawn-title"))
+        .description(LOCALES.lookup(&US_ENGLISH, "pokemon-spawn-description"))
+        .image(ImageSource::attachment("pokemon.png")?);
+
+    Ok(embed.validate()?.build())
+}
+
 async fn spawn_pokemon(
     client: &CommandClient<'_, State>,
     channel_id: Id<ChannelMarker>,
 ) -> Result<()> {
     let variant_id = 192;
     let image = get_spawn_image(client, variant_id).await?;
-    let attachments = [Attachment::from_bytes("pokemon.png".into(), image, 0)];
 
     let state = client.state.lock().await;
     let mut conn = state.redis.get().await?;
     conn.hset("wild", channel_id.get(), variant_id).await?;
 
-    client.http.create_message(channel_id).attachments(&attachments)?.exec().await?;
+    client
+        .http
+        .create_message(channel_id)
+        .attachments(&[Attachment::from_bytes("pokemon.png".into(), image, 0)])?
+        .embeds(&[make_spawn_embed()?])?
+        .exec()
+        .await?;
 
     Ok(())
 }
--- resources/locales/en/catching.ftl
+++ resources/locales/en/catching.ftl
@@ -1,2 +1,5 @@
 pokemon-caught = Congratulations {$user-mention}! You caught a level {$level} {$pokemon}!
 pokemon-caught-shiny = These colors seem unusual... ✨
+
+pokemon-spawn-title = A wild Pokémon has appeared!
+pokemon-spawn-description = Guess the Pokémon and run `/catch pokemon: <pokemon>` to catch it!
