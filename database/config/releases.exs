ef6ecffb88b4fb061507349eaa1eb0380acc886e
Oliver Ni <oliver.ni@gmail.com>
2022-06-19 14:50:12 -0700

    Add ssl option

--- database/config/releases.exs
+++ database/config/releases.exs
@@ -1,3 +1,13 @@
 import Config
 
 config :poketwo_database, Poketwo.Database.Repo, url: System.fetch_env!("DB_URL")
+
+if System.get_env("DB_CA_CERT") != nil do
+  config :poketwo_database, Poketwo.Database.Repo,
+    ssl: true,
+    ssl_opts: [
+      verify: :verify_peer,
+      cacertfile: System.get_env("DB_CA_CERT"),
+      verify_fun: &:ssl_verify_hostname.verify_fun/3
+    ]
+end
