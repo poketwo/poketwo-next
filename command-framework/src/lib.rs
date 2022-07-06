7f2f81be09632dc405386658fc0fd4c733cde2a6
Oliver Ni <oliver.ni@gmail.com>
2022-07-06 21:54:41 +0200

    Remove unneeded annotation

--- command-framework/src/lib.rs
+++ command-framework/src/lib.rs
@@ -4,8 +4,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#![feature(associated_type_defaults)]
-
 pub mod client;
 pub mod command;
 pub mod context;
